c07f57e04c38ea10cb07395b9173d572ee2bf997
ndebrasi <debrasi.nahuel@gmail.com>
2025-10-08 11:41:25 -0300

    Extend md and change in top parameters

--- PWM/notes/pwm.md
+++ PWM/notes/pwm.md
@@ -18,3 +18,40 @@ Se obtiene la siguiente señal de salida del filtro RC que pareceria estar sobre
 Cambiando la resolucion del dac de 12 a 16 bits y un RC = 2000E-6, se obtiene la siguiente señal:
 
 ![pwm_signal](pwm_rc_filter.png)
+
+---
+Cambiando 
+- parameter NCO_FREQ_BITS = 12;
+- RC = 1000E-6
+
+Se obtiene la siguiente señal:
+
+![pwm_signal](pwm_rc_filter5.png)
+
+
+---
+Cambiando 
+- parameter NCO_FREQ_BITS = 12;
+- RC = 2000E-6
+
+Se obtiene la siguiente señal:
+
+![pwm_signal](pwm_rc_filter2.png)
+
+---
+Cambiando 
+- parameter NCO_FREQ_BITS = 13;
+- RC = 1000E-6
+
+Se obtiene la siguiente señal:
+
+![pwm_signal](pwm_rc_filter3.png)
+
+---
+Cambiando 
+- parameter NCO_FREQ_BITS = 13;
+- RC = 2000E-6
+
+Se obtiene la siguiente señal:
+
+![pwm_signal](pwm_rc_filter4.png)
--- PWM/top.v
+++ PWM/top.v
@@ -10,31 +10,31 @@ module top (
     output wire mem_clk_out
 );
     // Parámetros
-    parameter NCO_BITS = 10;
+    parameter NCO_FREQ_BITS = 12;
     parameter ADDR_BITS = 10;
     parameter DATA_OUT_BITS = 16;
 
     // Salidas intermedias
+    wire [NCO_FREQ_BITS-1:0] nco_out;
     wire [ADDR_BITS-1:0] addr;
-    wire [NCO_BITS-1:0] nco_out;
     wire [DATA_OUT_BITS-1:0] data_out;
 
     // Instancia del NCO para la frecuencia
     nco #(
-        .NCO_BITS(NCO_BITS)
+        .NCO_BITS(NCO_FREQ_BITS)
     ) nco_freq (
         .rst_in(rst_in),
         .clk_in(clk_in),
         .nco_out(nco_out)
     );
-    assign mem_clk_out = nco_out[NCO_BITS-1]; // Salida del bit más significativo del NCO
+    assign mem_clk_out = nco_out[NCO_FREQ_BITS-1]; // Salida del bit más significativo del NCO
 
     // Instancia del NCO para la dirección
     nco #(
-        .NCO_BITS(NCO_BITS)
+        .NCO_BITS(ADDR_BITS)
     ) nco_addr (
         .rst_in(rst_in),
-        .clk_in(nco_out[NCO_BITS-1]), // Usamos el bit más significativo del primer NCO como reloj
+        .clk_in(nco_out[NCO_FREQ_BITS-1]), // Usamos el bit más significativo del primer NCO como reloj
         .nco_out(addr)
     );
 
