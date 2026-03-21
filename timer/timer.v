2bbb49233986630cf7d58effb1b97d0fd213894a
ndebrasi <debrasi.nahuel@gmail.com>
2026-03-21 18:26:19 -0300

    Add timer directory

---
+++ timer/timer.v
@@ -0,0 +1,15 @@
+module timer (
+    input wire clk,
+    input wire signalIn,
+    output reg ledRed,
+    output reg ledGreen,
+    output reg ledYellow,
+    output reg signalOut,
+    output reg groundOut
+);
+
+
+
+
+
+endmodule
